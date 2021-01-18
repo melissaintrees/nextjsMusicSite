bcdce8d8e71c30e4c6bed0ae20ef41c562eeedf3
Melissa St Moore <melissaintrees@gmail.com>
2021-01-17 20:38:34 -0600

    fixing env var for nextjs part deux

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        YT_API_KEY='AIzaSyBneax5Jo0iVKlNDypDsEETTdEQ0doM1y8',
-        BAF_PLAYLIST_ID='PLcd_6XBjr83efNqUpgp9vEr1zgwLLhBpj'
+        YT_API_KEY: 'AIzaSyBneax5Jo0iVKlNDypDsEETTdEQ0doM1y8',
+        BAF_PLAYLIST_ID: 'PLcd_6XBjr83efNqUpgp9vEr1zgwLLhBpj'
     },
 }
