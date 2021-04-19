9b7fb533c59516d0028798fa32656119d06203e8
Melissa St Moore <melissaintrees@gmail.com>
2021-04-18 20:24:22 -0500

    changed out photo

--- pages/info.js
+++ pages/info.js
@@ -17,8 +17,7 @@ export default function MyInfoPage(props) {
             </Head>
             <Col lg={6}>
                 <h1 className={styles.pagesHeading}>{pageName}</h1>
-                <Image className={styles.coverImage} title="hello" src="/images/Melissa-Retouch-Tea-alien-BrandonRizzuto.jpg" alt="woman, Melissa, pours tea for alien friend, Fran" fluid></Image>
-
+                <Image className={styles.coverImage} title="hello" src="/images/melissa-in-trees-1993-5by7-v1.jpg" alt="This picture is from the early-mid 90s at the Michigans Women's Music Festival. It is of Melissa, a young white girl with long brown hair in a tree." fluid></Image>
             </Col>
             <Col lg={6}>
                 <p className="infoText">Melissa In Trees is a singer, music producer and content creator from Los Angeles, CA. She creates eccentric dream pop, indy rock, sing song raps, that are influenced by surreal darkness & realistic brightness.
@@ -26,9 +25,7 @@ export default function MyInfoPage(props) {
                 Besides music, she owns a web development company, St Moore Dev, that focuses on using technology to help people, and hosts a good news program called Bananas AF through her YouTube channel, along with Videos, Covers, and Web Tutorials.
                 <br />
                     <br />
-
                 Melissa released an 8 song debut album which is being released as singles over the course of 2021.
-
                 </p>
             </Col>
             <style jsx>{`
