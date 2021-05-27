99248563f0cf4121fc87b48fc8cec2bf2f277cc2
Melissa St Moore <melissastmoore@Melissas-MacBook-Pro.local>
2021-05-27 13:02:37 -0500

    changing spotify link

--- components/mynav.js
+++ components/mynav.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import styles from './layout.module.css'
-import {Navbar, Nav} from 'react-bootstrap'
+import { Navbar, Nav } from 'react-bootstrap'
 import ReactAudioPlayer from 'react-audio-player';
 
 export const MyNav = () => {
@@ -10,51 +10,55 @@ export const MyNav = () => {
 
   return (
     <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-  {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    {/* <Nav> */}
-    <Nav className={styles.headerChild} activeKey="/home">
-    {/* <Nav.Item className={router.pathname == "/" ? `${styles.highlight}` : ""}>
+      {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        {/* <Nav> */}
+        <Nav className={styles.headerChild} activeKey="/home">
+          {/* <Nav.Item className={router.pathname == "/" ? `${styles.highlight}` : ""}>
       <Nav.Link href="/">music</Nav.Link>
     </Nav.Item> */}
-    <Link href="/">
-      <a className={router.pathname == "/" ? `${styles.highlight}` : ""} >
-        music
+          <Link href="/">
+            <a className={router.pathname == "/" ? `${styles.highlight}` : ""} >
+              home
       </a>
-    </Link>
-    <Link href="/info">
-        <a className={router.pathname == "/info" ? `${styles.highlight}` : ""} >
-          info
-        </a>
-    </Link>
-    <Link href="/videos">
-      <a className={router.pathname == "/videos" ? `${styles.highlight}` : ""} >
-        videos
-      </a>
-    </Link>
-    <Link href="/show">
-      <a className={router.pathname == "/show" ? `${styles.highlight}` : ""} >
-        show
-      </a>
-    </Link>
-    
-    <div className={styles.socialGrp}>
-      
-      <Nav.Item>
-              <a href="https://mailchi.mp/bacbde16cf94/mailinglist" ><i className="far fa-newspaper"></i></a>
-              <a href="https://www.instagram.com/melissastmoore" ><i className="fab fa-instagram"></i></a>
-              <a href="http://www.youtube.com/melissastmoore" ><i className="fab fa-youtube"></i></a>
-              <a href="https://artists.spotify.com/c/artist/4RUivryHDsx8Q9s6kcs3YP/profile/overview" ><i className="fab fa-spotify"></i></a>
-      </Nav.Item> 
- 
-    </div> 
-     
-  </Nav>
+          </Link>
 
-  
-  </Navbar.Collapse>
-</Navbar>
+          <a href="https://smarturl.it/MaDebut">Listen</a>
+
+          {/* <Link href="https://smarturl.it/MaDebut"></Link> */}
+          <Link href="/info">
+            <a className={router.pathname == "/info" ? `${styles.highlight}` : ""} >
+              info
+        </a>
+          </Link>
+          <Link href="/videos">
+            <a className={router.pathname == "/videos" ? `${styles.highlight}` : ""} >
+              videos
+      </a>
+          </Link>
+          <Link href="/show">
+            <a className={router.pathname == "/show" ? `${styles.highlight}` : ""} >
+              show
+      </a>
+          </Link>
+
+          <div className={styles.socialGrp}>
+
+            <Nav.Item>
+              <a href="https://mailchi.mp/bacbde16cf94/mailinglist" ><i className="far fa-newspaper"></i></a>
+              <a href="https://www.instagram.com/melissa_in_trees" ><i className="fab fa-instagram"></i></a>
+              <a href="http://www.youtube.com/melissastmoore" ><i className="fab fa-youtube"></i></a>
+              <a href="https://open.spotify.com/artist/4RUivryHDsx8Q9s6kcs3YP" ><i className="fab fa-spotify"></i></a>
+            </Nav.Item>
+
+          </div>
+
+        </Nav>
+
+
+      </Navbar.Collapse>
+    </Navbar>
 
   );
 };
--- pages/info.js
+++ pages/info.js
@@ -20,9 +20,9 @@ export default function MyInfoPage(props) {
                 <Image className={styles.coverImage} title="hello" src="/images/melissa-in-trees-1993-5by7-v1.jpg" alt="This picture is from the early-mid 90s at the Michigans Women's Music Festival. It is of Melissa, a young white girl with long brown hair in a tree." fluid></Image>
             </Col>
             <Col lg={6}>
-                <p className="infoText">Melissa In Trees is a singer, music producer and content creator from Los Angeles, CA. She creates eccentric dream pop, indy rock, sing song raps, that are influenced by surreal darkness & realistic brightness.
+                <p className="infoText">Melissa St Moore (FKA SKATES) is a singer, music producer and content creator from Los Angeles, CA. She creates eccentric dream pop, indy rock, sing song raps, that are influenced by surreal darkness & realistic brightness.
                 <br /><br />
-                Besides music, she owns a web development company, St Moore Dev, that focuses on using technology to help people, and hosts a good news program called Bananas AF through her YouTube channel, along with Videos, Covers, and Web Tutorials.
+                Besides music, she owns a web development company, St Moore Dev, that focuses on using technology to help people, and hosts a Good News program called Bananas AF through her YouTube channel, along with Videos, Covers, and Web Tutorials.
                 <br />
                     <br />
                 Melissa released an 8 song debut album which is being released as singles over the course of 2021.
@@ -30,13 +30,14 @@ export default function MyInfoPage(props) {
             </Col>
             <style jsx>{`
                 .infoText {
-                    margin: 15% 0px 15% 0px;
+                    margin: 10% 0px;
                     text-align: left;
                     font-size: 16px;
                     color: black;
                     font-weight:500;
                     line-height: 30px;
                     font-family: 'Roboto', sans-serif;
+              
                 }
 
              
